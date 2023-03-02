41e5aec7b5c8bbd7dd609f0ed2c3ea9f3a135b1d
Karl Bateman <karl.bateman@pm.me>
2023-03-02 11:29:19 +0000

    chore: tidy handlers

--- handlers.go
+++
@@ -1,6 +0,0 @@
-package astro
-
-// Handlers registers handlers which respond to messages received from STDIN.
-func (s *Server) Handlers() {
-	s.HandleEcho()
-}
--- server.go
+++ server.go
@@ -7,18 +7,17 @@ import (
 // New creates a new Astro server object.
 func New() *Server {
 	s := &Server{Node: maelstrom.NewNode()}
-	s.Handlers()
+	s.HandleEcho()
 	return s
 }
-
 
 // Server wraps an Astro server and it's dependencies.
 type Server struct {
 	Node *maelstrom.Node
 }
 
-// Run launches the Astro servers main event-handling loop. It reads messages 
-// from STDIN and delegates them to their corresponding registered handler. 
+// Run launches the Astro servers main event-handling loop. It reads messages
+// from STDIN and delegates them to their corresponding registered handler.
 func (s *Server) Run() error {
 	return s.Node.Run()
 }
